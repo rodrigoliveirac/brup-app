5f949e4769479da275e85c7ff921d220b4f8aa35
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2023-11-05 18:29:10 -0300

    Initial commit

---
+++ src/test/java/com/brupapp/rodcollab/RodcollabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brupapp.rodcollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodcollabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
